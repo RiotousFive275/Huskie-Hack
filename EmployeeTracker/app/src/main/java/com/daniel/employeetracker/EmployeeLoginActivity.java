ef66e954647dd672eec98013a097898c7ab031e2
Polakmaly <danieljablonski10@gmail.com>
2016-11-05 19:21:35 -0500

    Made new app
    Added new app

--- EmployeeTracker/app/src/main/java/com/daniel/employeetracker/EmployeeLoginActivity.java
+++ EmployeeTracker/app/src/main/java/com/daniel/employeetracker/EmployeeLoginActivity.java
@@ -1,14 +1,13 @@
-package com.daniel.hhp;
+package com.daniel.employeetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class EmployeeLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_employee_login);
     }
 }
